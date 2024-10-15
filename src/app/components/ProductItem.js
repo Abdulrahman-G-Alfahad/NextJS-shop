c28bf132c1b91e9468b2d13f01bf5e5e8b8844d9
unknown <abdulrahman.g.alfahad@gmail.com>
2024-10-15 13:38:43 +0300

    Completed Task 4

---
+++ src/app/components/ProductItem.js
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import products from "../products";
+
+function ProductItem() {
+  //   const productList = products.map((product) => {
+  //     return (
+
+  //     );
+  //   });
+
+  return (
+    <div
+      key={products[0].name}
+      className="flex flex-col items-center border border-gray-300 rounded-lg p-4 shadow-lg"
+    >
+      <Image
+        src={products[0].imageSrc}
+        alt={products[0].name}
+        width={200}
+        height={200}
+      />
+      <p>{products[0].name}</p>
+      <p>{products[0].price}</p>
+    </div>
+  );
+}
+
+export default ProductItem;
+{
+  /* <div className="grid grid-cols-3 gap-4">{productList}</div>; */
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import bookShopImage from "../../src/images/bookshopimage.jpg";
-import products from "./products.js";
+import ProductItem from "./components/ProductItem";
 
 export default function Home() {
   //TASK 2
@@ -43,28 +43,38 @@ export default function Home() {
   // );
 
   //Task 3
-  const productList = products.map((product) => {
-    return (
-      <div className="flex flex-col items-center">
-        <Image
-          src={product.imageSrc}
-          alt={product.name}
-          width={200}
-          height={200}
-        />
-        <p>{product.name}</p>
-        <p>{product.price}</p>
-      </div>
-    );
-  });
+
+  // const productList = products.map((product) => {
+  //   return (
+  //     <div
+  //       key={product.name}
+  //       className="flex flex-col items-center border border-gray-300 rounded-lg p-4 shadow-lg"
+  //     >
+  //       <Image
+  //         src={product.imageSrc}
+  //         alt={product.name}
+  //         width={200}
+  //         height={200}
+  //       />
+  //       <p>{product.name}</p>
+  //       <p>{product.price}</p>
+  //     </div>
+  //   );
+  // });
 
   return (
     <div className="text-center">
+      <br></br>
       <h1 className="text-5xl"> Awesome Bookshop </h1>
+      <br></br>
       <h2 className="text-2xl"> Where you can find all of the awesome books</h2>
+      <br></br>
       <Image src={bookShopImage}></Image>
       <br></br>
-      <div className="grid grid-cols-3 gap-4">{productList}</div>
+      <br></br>
+      <ProductItem />
+      <div className="grid grid-cols-3 gap-4"></div>
+      <br></br>
     </div>
   );
 }
