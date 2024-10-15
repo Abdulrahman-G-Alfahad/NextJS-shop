040e623094f35125429585d68f47177a2d02136f
unknown <abdulrahman.g.alfahad@gmail.com>
2024-10-15 12:02:31 +0300

    Completed Task 1 and Task 2

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,56 @@
 import Image from "next/image";
+import bookShopImage from "../../src/images/bookshopimage.jpg";
+import products from "./products.js";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const product1 = (
+    <div className="flex flex-col items-center">
+      <Image
+        src={products[0].imageSrc}
+        alt={products[0].name}
+        width={200}
+        height={200}
+      />
+      <p>{products[0].name}</p>
+      <p>{products[0].price}</p>
+    </div>
+  );
+  const product2 = (
+    <div className="flex flex-col items-center">
+      <Image
+        src={products[1].imageSrc}
+        alt={products[1].name}
+        width={200}
+        height={200}
+      />
+      <p>{products[1].name}</p>
+      <p>{products[1].price}</p>
+    </div>
+  );
+  const product3 = (
+    <div className="flex flex-col items-center">
+      <Image
+        src={products[2].imageSrc}
+        alt={products[2].name}
+        width={200}
+        height={200}
+      />
+      <p>{products[2].name}</p>
+      <p>{products[2].price}</p>
+    </div>
+  );
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  return (
+    <div className="text-center">
+      <h1 className="text-5xl"> Awesome Bookshop </h1>
+      <h2 className="text-2xl"> Where you can find all of the awesome books</h2>
+      <Image src={bookShopImage}></Image>
+      <br></br>
+      <div className="grid grid-cols-3 gap-4">
+        <div>{product1}</div>
+        <div>{product2}</div>
+        <div>{product3}</div>
+      </div>
     </div>
   );
 }
---
+++ src/app/products.js
@@ -0,0 +1,23 @@
+import bookImage from "../../src/images/LOTR-1.webp";
+import book2Image from "../../src/images/LOTR-2.jpg";
+import book3Image from "../../src/images/LOTR-3.jpg";
+
+const products = [
+  {
+    name: "Lord of the ring: Fellowship of the ring",
+    imageSrc: bookImage,
+    price: "5 KD",
+  },
+  {
+    name: "Lord of the ring: The two towers",
+    imageSrc: book2Image,
+    price: "5 KD",
+  },
+  {
+    name: "Lord of the ring: Return of the king",
+    imageSrc: book3Image,
+    price: "5 KD",
+  },
+];
+
+export default products;
